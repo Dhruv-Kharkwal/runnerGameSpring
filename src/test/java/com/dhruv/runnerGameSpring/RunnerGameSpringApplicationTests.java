f1f9f9957e2dd60eafb8921001f2f53fcfa4f4a5
DhruvKharkwal <dhruvkharkwal@gmail.com>
2023-09-24 00:39:58 +0530

    Project initialization

---
+++ src/test/java/com/dhruv/runnerGameSpring/RunnerGameSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruv.runnerGameSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerGameSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
